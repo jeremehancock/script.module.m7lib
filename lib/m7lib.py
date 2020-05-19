30adae8eac271e3f11008d781f5d3f4d79f3e885
Jereme <michael@jeremehancock.com>
2020-05-18 22:29:43 -0500

    update with patreon info

--- lib/m7lib.py
+++ lib/m7lib.py
@@ -1,5 +1,7 @@
 """
     m7lib
+    Developed by mhancoc7
+    https://patreon.m7kodi.dev
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
